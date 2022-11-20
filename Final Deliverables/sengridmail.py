25daf32082002fb6cf88078f027c1b8a862237ae
Yaazhini_periasamy <88047197+Yaazhini18@users.noreply.github.com>
2022-11-20 11:14:02 +0530

    Update sengridmail.py

--- Final Deliverables/sengridmail.py
+++ Final Deliverables/sengridmail.py
@@ -6,7 +6,7 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 def sendmail(usermail,subject,name,content):
-    message = Mail(from_email='rukirukash24@gmail.com',to_emails=usermail,subject=subject,html_content='<h4>Hello {}, </h4><br/><strong> {} </strong><br/><p>Best Wishes,</p><p>Team Plasma</p>'.format(name,content))
+    message = Mail(from_email='73151913106@smartinternz.com',to_emails=usermail,subject=subject,html_content='<h4>Hello {}, </h4><br/><strong> {} </strong><br/><p>Best Wishes,</p><p>Team Plasma</p>'.format(name,content))
     try:
         sg = SendGridAPIClient(os.getenv('API_KEY'))
         response = sg.send(message)
